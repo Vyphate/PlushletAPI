693bbfad1e66001f20d735b88a83da072569d7f1
Vyphate <106931547+Vyphate@users.noreply.github.com>
2022-06-06 19:40:48 +0100

    Update host.lua

--- host.lua
+++ host.lua
@@ -1,1 +1,12 @@
-print("Hello")
+local PlushletAPI = {}
+PlushletAPI = PlushletAPI.__index
+
+function PlushletAPI.new(Key)
+  local self = setmetatable({}, PlushletAPI)
+  
+  self.Key = Key .. "/PlushletAPI"
+  
+  return self
+end
+
+return PlushletAPI
